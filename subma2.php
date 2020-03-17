23b37f27a4cf5a46372129e133a278f628970da5
submateam <61092073+submateam@users.noreply.github.com>
2020-03-18 01:31:51 +0700

    subma2

--- subma2.php
+++ subma2.php
@@ -104,7 +104,7 @@ echo color("green"," =================================== \n");
 						sleep(1);
 					}
 					sleep(3);
-					$goride1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"HEPIBROS"}');
+					$goride1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOODLAGI090320A"}');
 					$message2 = fetch_value($goride1,'"message":"','"');
 					echo "\n".color("green","+] Message: ".$message2);
 					sleep(3);
