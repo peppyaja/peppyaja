acdfedfc747fe8a359bd0cd168368ac5413ca1e0
submateam <61092073+submateam@users.noreply.github.com>
2020-03-31 16:47:59 +0700

    subma2

--- subma2.php
+++ subma2.php
@@ -4,10 +4,10 @@ include "function.php";
 echo "\e[92m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n";
 echo "\e[92m| __\e[93mDaftar\e[92m_\e[93mAkun\e[92m_\e[93m+\e[92m_\e[93mAuto\e[92m_\e[93mClaim\e[92m_\e[93mVoucher\e[92m_\e[93mFood\e[92m__|\n";
 echo "\e[92m| ___________\e[93m•_+\e[92m_\e[93mBuat\e[92m_\e[93mPIN\e[92m_\e[93m-_•\e[92m______________|\n";
-echo "\e[92m| _\e[91mUNTUK\e[92m_\e[91mREGISTRASI\e[92m_\e[91mSILAHKAN\e[92m_\e[91mHUBUNGI\e[92m_\e[91mRtiga\e[92m_|\n";
+echo "\e[92m| _\e[91mUNTUK\e[92m_\e[91mREGISTRASI\e[92m_\e[91mSILAHKAN\e[92m_\e[91mHUBUNGI\e[92m_\e[91mkurang beruntung chanel\e[92m_|\n";
 echo "\e[92m| ___\e[93mTELEGRAM\e[92m_\e[91m:kurang beruntung chanel\e[92m___________________|\n";
 echo "\e[92m| ___\e[93mGITHUB\e[92m___\e[91m: subcribe chanelnya ya\e[92m___________|\n";
-echo "\e[92m| ___\e[93mVersion\e[92m__\e[91m:∆Gojek\e[92m_\e[93m-\e[92m_\e[93mVersi 3.48.2\e[92m_\e[93m21∆\e[92m____________|\n";
+echo "\e[92m| ___\e[93mVersion\e[92m__\e[91m:user login klik y atau sudah\e[92m_\e[93m-\e[92m_\e[93mVersi 3.48.2\e[92m_\e[93m21∆\e[92m____________|\n";
 echo "\e[92m| ___\e[93mTime\e[92m_____\e[93m:".date('[d-m-Y] [H:i:s]')."\e[92m_____|\n";
 echo "\e[92m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n";
 	echo "\n";
@@ -23,7 +23,7 @@ $pass = ( '║ User Login : ' );
 echo "\033[30;40m";
 $user = trim(fgets(STDIN));
 echo "\033[0m";
-if($user == "Rtiga")
+if($user == "subma")
 {
 	echo "\e[92m╚══════════════════════════════════╝\n";
 	echo "\e[92m╔══════════════════════════════════╗\n";
